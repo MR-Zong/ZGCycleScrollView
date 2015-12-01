194e33713cd0033519fafdf4013c4a8c58d9b13f
徐宗根 <zong@xuzonggendeMacBook-Pro.local>
2015-12-01 20:32:38 +0800

    initilize

---
+++ ZgImageRecyclePlayer/ZGImageRecyclePlayer/ZGImageRecyclePlayerViewController.m
@@ -0,0 +1,342 @@
+//
+//  ZGImageRecyclePlayerViewController.m
+//  ZGWuXianLunBoQi
+//
+//  Created by 徐宗根 on 15/12/1.
+//  Copyright (c) 2015年 XuZonggen. All rights reserved.
+//
+
+#import "ZGImageRecyclePlayerViewController.h"
+
+#define OBJKEY(obj,key) ((void)obj.key,@(#key))
+#define ZGPAGECONTROLHEIGHT 20
+// 颜色
+#define ZGCOLOR_32(a,r,g,b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
+#define ZGCOLOR_24(r,g,b) ZGCOLOR_32(1.0,(r),(g),(b))
+#define ZGGRAYCOLOR(v) ZGCOLOR_24((v),(v),(v))
+#define ZGBACKGROUNDCOLOR ZGGRAYCOLOR(215)
+#define ZGRandomColor ZGCOLOR_32(1.0,arc4random_uniform(255),arc4random_uniform(255),arc4random_uniform(255))
+
+
+
+@interface ZGImageRecyclePlayerViewController ()<UIScrollViewDelegate>
+
+
+
+/** scrollView */
+@property (nonatomic,strong) UIScrollView *sv;
+
+@property (nonatomic,strong) NSMutableSet *imageViewSet;
+
+@property (nonatomic,strong) UIImageView *curImageView;
+
+@property (nonatomic,strong) UIImageView *tmpImageView;
+
+
+@property (nonatomic,assign) BOOL stopFlag;
+
+
+@property (nonatomic,assign) BOOL pageFlag;
+
+@property (nonatomic,copy) NSArray *images;
+
+@property (nonatomic,assign) CGRect viewFrame;
+
+@property (nonatomic,assign) NSInteger imageIndex;
+
+
+/** pageContoller */
+@property (nonatomic,strong) UIPageControl *pageControl;
+
+/** timer */
+@property (nonatomic,strong) NSTimer *timer;
+
+
+@end
+
+
+
+@implementation ZGImageRecyclePlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    // 至关重要
+    self.view.frame = self.viewFrame;
+    
+    UIScrollView *sv = [[UIScrollView alloc] init];
+    self.sv = sv;
+ 
+    sv.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    //    sv.backgroundColor = [UIColor redColor];
+    sv.pagingEnabled = YES;
+    sv.contentOffset = CGPointMake(sv.frame.size.width, 0);
+    sv.showsHorizontalScrollIndicator = NO;
+    sv.delegate = self;
+    [sv addObserver:self forKeyPath:OBJKEY(sv, contentOffset) options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+    
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(sv.frame.size.width, 0, sv.frame.size.width, sv.frame.size.height)];
+    self.curImageView = imgView;
+    
+    imgView.backgroundColor = [UIColor whiteColor];
+    
+    [sv addSubview:imgView];
+    
+    sv.contentSize = CGSizeMake(sv.frame.size.width * (sv.subviews.count + 2), 0);
+    
+    [self.view addSubview:sv];
+    
+    /** pageControl */
+    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, sv.frame.size.height - 20, sv.frame.size.width, 20)];
+    self.pageControl = pageControl;
+    
+    pageControl.pageIndicatorTintColor = [UIColor grayColor];
+    pageControl.numberOfPages = 3;
+    
+    /** pageControl 居中*/
+    pageControl.hidesForSinglePage = YES;
+    //        pageControl.contentMode = UIViewContentModeCenter;
+    //        pageControl.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [self.view addSubview:pageControl];
+    
+    
+    
+    self.stopFlag = YES;
+    
+    self.pageFlag = NO;
+    
+    self.imageIndex = 1;
+    
+    [self timerStart];
+    
+}
+
+
++ (instancetype)imageRecyclePlayerViewControllerWithImages:(NSArray *)images Frame:(CGRect)frame
+{
+    ZGImageRecyclePlayerViewController *recycleViewControler = [[ZGImageRecyclePlayerViewController alloc] init];
+    
+    recycleViewControler.images = images;
+    recycleViewControler.viewFrame = frame;
+    
+    return recycleViewControler;
+}
+
+- (void)dealloc
+{
+    [self.sv removeObserver:self forKeyPath:OBJKEY(self.sv, contentOffset)];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    
+    if (self.images.count) {
+        
+        self.curImageView.image = self.images[0];
+    }
+    self.pageControl.numberOfPages = self.images.count;
+    if (self.images.count <= 1) {
+        [self timerStop];
+        self.sv.delegate = nil;
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            [self.sv removeObserver:self forKeyPath:OBJKEY(self.sv, contentOffset)];
+        });
+        
+        self.curImageView.frame = CGRectMake(0, 0, self.sv.frame.size.width, self.sv.frame.size.height);
+        self.sv.contentSize = CGSizeMake(self.sv.frame.size.width, 0);
+    }
+    
+}
+
+
+#pragma mark - lazyLoad
+- (NSMutableSet *)imageViewSet
+{
+    if (!_imageViewSet) {
+        _imageViewSet = [NSMutableSet set];
+    }
+    return _imageViewSet;
+}
+
+- (void)setImageIndex:(NSInteger)imageIndex
+{
+    NSLog(@"imageIndex %zd,images.count %zd",imageIndex,self.images.count);
+    
+    if (self.images.count && imageIndex >= self.images.count) {
+        imageIndex = 0;
+    }
+    _imageIndex = imageIndex;
+    
+}
+
+
+#pragma mark - sv.contentOffset KVO
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([change[@"new"] CGPointValue].x  == [change[@"old"] CGPointValue].x)
+        return;
+    
+    
+    if (self.stopFlag) {
+        
+        UIImageView *imgView = [self dequeueImageView];
+        if (imgView) {
+            imgView.backgroundColor = ZGRandomColor;
+            NSLog(@"从缓存池取出的");
+        }
+        
+        if(!imgView)
+        {
+            imgView = [[UIImageView alloc] init];
+            [self.imageViewSet addObject:imgView];
+            
+            imgView.backgroundColor = [UIColor blackColor];
+            NSLog(@"缓存池没有，要创建一个");
+        }
+        
+        
+        if ( [change[@"new"] CGPointValue].x  > [change[@"old"] CGPointValue].x) {
+            NSLog(@"往左滑    <<");
+            
+            
+            imgView.frame = CGRectMake(self.curImageView.frame.origin.x + self.sv.frame.size.width, 0, self.sv.frame.size.width, self.sv.frame.size.height);
+            
+            self.stopFlag = NO;
+            
+            imgView.image = self.images[self.imageIndex];
+            self.tmpImageView = imgView;
+            [self.sv addSubview:imgView];
+            // 从缓存池移除
+            [self.imageViewSet removeObject:self.tmpImageView];
+            
+            
+        }else if( [change[@"new"] CGPointValue].x  < [change[@"old"] CGPointValue].x){
+            NSLog(@"往右滑     >>");
+            
+            
+            
+            imgView.frame = CGRectMake(self.curImageView.frame.origin.x - self.sv.frame.size.width, 0, self.sv.frame.size.width, self.sv.frame.size.height);
+            
+            self.stopFlag = NO;
+            
+            imgView.image = self.images[self.imageIndex];
+            self.tmpImageView = imgView;
+            [self.sv addSubview:imgView];
+            [self.imageViewSet removeObject:self.tmpImageView];
+            
+        }
+    }
+    
+    
+}
+
+
+- (UIImageView *)dequeueImageView
+{
+    return [self.imageViewSet anyObject];
+}
+
+#pragma mark - <UIScrollViewDelegate>
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self timerStop];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    if(scrollView.contentOffset.x >= self.sv.frame.size.width * 0.5 && scrollView.contentOffset.x <= self.sv.frame.size.width *1.5 )
+    {
+        self.pageFlag = NO;
+    }else{
+        self.pageFlag = YES;
+    }
+    
+    NSLog(@"scrollViewDidEndDragging");
+}
+
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    
+    if (self.pageFlag == NO) {
+        [self.imageViewSet addObject:self.tmpImageView];
+        [self.tmpImageView removeFromSuperview];
+        self.tmpImageView = nil;
+        
+        //        NSLog(@"contentOffset %@",NSStringFromCGPoint(self.sv.contentOffset));
+        // 一定要加 因为系统的scrollView pageEnable 会把contentOffset 修改错，要把它调回来
+        [self.sv setContentOffset:CGPointMake(self.sv.frame.size.width, 0)];
+        
+        
+    }else{
+        
+        [self.imageViewSet addObject:self.curImageView];
+        [self.curImageView removeFromSuperview];
+        self.curImageView = self.tmpImageView;
+        self.tmpImageView = nil;
+        
+        CGRect tmpRect = self.curImageView.frame;
+        tmpRect.origin.x = self.sv.frame.size.width;
+        self.curImageView.frame = tmpRect;
+        NSLog(@"self.curImageView.frame %@",NSStringFromCGRect(self.curImageView.frame));
+        [self.sv setContentOffset:CGPointMake(self.sv.frame.size.width, 0)];
+        // pageFlage == YES 才能加
+        self.imageIndex += 1;
+    }
+    
+    
+    
+    self.pageControl.currentPage = [self.images indexOfObject:self.curImageView.image];
+    self.stopFlag = YES;
+    [self timerStart];
+    
+    NSLog(@"scrollViewDidEndDecelerating");
+    
+    
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
+{
+    
+    [self.imageViewSet addObject:self.curImageView];
+    [self.curImageView removeFromSuperview];
+    self.curImageView = self.tmpImageView;
+    self.tmpImageView = nil;
+    
+    CGRect tmpRect = self.curImageView.frame;
+    tmpRect.origin.x = self.sv.frame.size.width;
+    self.curImageView.frame = tmpRect;
+    [self.sv setContentOffset:CGPointMake(self.sv.frame.size.width, 0)];
+    
+    self.imageIndex += 1;
+    self.pageControl.currentPage = [self.images indexOfObject:self.curImageView.image];
+    self.stopFlag = YES;
+    
+    NSLog(@"scrollViewDidEndScrollingAnimation");
+}
+
+#pragma mark - Timer
+- (void)timerStart
+{
+    self.timer = [NSTimer timerWithTimeInterval:2 target:self selector:@selector(doTimer) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+- (void)timerStop
+{
+    [self.timer invalidate];
+}
+
+- (void)doTimer
+{
+    [self.sv setContentOffset:CGPointMake(self.sv.contentOffset.x + self.sv.frame.size.width, 0) animated:YES];
+}
+
+
+
+
+
+@end
